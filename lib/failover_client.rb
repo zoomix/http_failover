3081c83af6fd5bc542897dbdf19766f9f4cd06d5
Srdan Kvrgic <srdan@burtcorp.com>
2013-05-09 23:55:10 +0200

    Timeouts

--- lib/failover_client.rb
+++ lib/failover_client.rb
@@ -1,14 +1,28 @@
 require 'httparty'
 
 class FailoverClient
-  def initialize(base_uris)
+  def initialize(base_uris, retry_timeouts = [1, 5, 5])
     @base_uris = base_uris
     @request_counter = 0
+    @retry_timeouts = retry_timeouts
+    @retry = 0
   end
 
   def get(uri, options={})
     url = get_base_uri + uri
-    response = HTTParty.get(url, options)
+    begin
+      response = HTTParty.get(url, options)
+      @retry = 0
+    rescue Timeout::Error => e
+      if @retry < @retry_timeouts.count 
+        sleep @retry_timeouts[@retry]
+        @retry += 1
+        response = get(uri)
+      else
+        raise e
+      end
+    end
+
     raise HttpClienError.new(response.code, response.body, response.headers) if (400..499).cover? response.code
     response
   end
--- spec/failover_client_spec.rb
+++ spec/failover_client_spec.rb
@@ -75,5 +75,35 @@ describe FailoverClient do
     end
   end
 
+  context 'Timeouts' do
+    let :failover_client do
+      FailoverClient.new(['http://apa', 'http://bepa:1337'], [0.1, 0.1, 0.1])
+    end
+
+    it 'tries the other server if the first times out' do
+      request_a = stub_request(:get, 'http://apa/something').to_timeout
+      request_b = stub_request(:get, 'http://bepa:1337/something')
+      failover_client.get('/something')
+      request_a.should have_been_requested
+      request_b.should have_been_requested
+    end
+
+    it 'keep retrying until it runs out of retries. Then throws the error' do
+      stub_request(:get, 'http://bepa:1337/something').to_timeout
+      stub_request(:get, 'http://apa/something').to_timeout
+      expect { failover_client.get('/something') }.to raise_error(Timeout::Error)
+    end
+
+    it 'gets the last' do
+      new_failover_client = FailoverClient.new(['http://apa', 'http://bepa', 'http://cepa', 'http://depa'], [0.1, 0.1, 0.1])
+      r_d = stub_request(:get, 'http://depa/something').to_return({status: 200, body: 'Finally. A response.'})
+      r_c = stub_request(:get, 'http://cepa/something').to_timeout
+      r_b = stub_request(:get, 'http://bepa/something').to_timeout
+      r_a = stub_request(:get, 'http://apa/something').to_timeout
+      response = new_failover_client.get('/something')
+      response.code.should == 200
+      response.body.should == 'Finally. A response.'
+    end
+  end
 
 end
